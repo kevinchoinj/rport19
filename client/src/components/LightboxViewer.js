df8bfe23d7bb98c5bbe101ddf92bf8f4ffcbe6ec
unknown <kevinchoinj@yahoo.com>
2019-05-03 21:30:55 -0400

    rewrote LightboxViewer as functional component; kinda janky looking, maybe rewrite later or split into smaller components

--- client/src/components/LightboxViewer.js
+++ client/src/components/LightboxViewer.js
@@ -1,162 +1,125 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Lightbox from 'react-images';
 import Scrollbar from 'smooth-scrollbar';
-import ReactDOM from 'react-dom';
 
-export default class Viewer extends React.Component{
-  state = {
-    lightboxIsOpen: false,
-    currentImage: 0,
-    isDown: false,
-    isMoving: false,
-    startX: 0,
-    scrollLeft: 0,
+const RenderGallery = ({images, openLightbox, scrollbar}) => {
+  if (!images) return;
+  const gallery = images.map((obj, i) => {
+    return (
+      <div
+        key={i}
+        className="gaming_carousel_object"
+        onLoad={()=>scrollbar.update()}
+        onClick={(e)=> openLightbox(i,e)}
+      >
+        <img
+          src={obj.src}
+          className="gaming_carousel_image"
+          alt="gaming screenshot"
+        />
+      </div>
+    );
+  });
+  return gallery;
+};
+
+const Viewer = ({images, carouselId}) => {
+
+  useEffect(() => {
+    const scrollbar = Scrollbar.init(document.querySelector(carouselId),
+      {
+        alwaysShowTracks: true,
+      });
+    setScrollbar(scrollbar);
+  }, []);
+
+  const [scrollbar, setScrollbar] = useState(false);
+  const goLeft = () => {
+    scrollbar.scrollTo(scrollbar.scrollLeft - window.innerWidth/3, 0, 600);
+  };
+  const goRight = () => {
+    scrollbar.scrollTo(scrollbar.scrollLeft + window.innerWidth/3, 0, 600);
   };
 
-  openLightbox = (index, event) =>{
-    if (this.state.isMoving) {
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const openLightbox = (index, event) => {
+    if (isMoving) {
       event.preventDefault();
-      this.setState({
-        isMoving: false,
-      });
+      setIsMoving(false);
     }
     else {
       event.preventDefault();
-      this.setState({
-        currentImage: index,
-        lightboxIsOpen: true,
-      });
+      setCurrentImage(index);
+      setLightboxOpen(true);
     }
-  }
-  closeLightbox = () => {
-    this.setState({
-      currentImage: 0,
-      lightboxIsOpen: false,
-    });
-  }
-  gotoPrevious = () => {
-    this.setState({
-      currentImage: this.state.currentImage - 1,
-    });
-  }
-  gotoNext = () => {
-    this.setState({
-      currentImage: this.state.currentImage + 1,
-    });
-  }
-  gotoImage = (index) => {
-    this.setState({
-      currentImage: index,
-    });
-  }
-  handleClickImage = () => {
-    if (this.state.currentImage === this.props.images.length - 1) return;
+  };
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setLightboxOpen(false);
+  };
 
-    this.gotoNext();
-  }
+  const [isMoving, setIsMoving] = useState(false);
+  const [currentImage, setCurrentImage] = useState(0);
 
-  componentDidMount() {
-    const scrollbar = Scrollbar.init(document.querySelector(this.props.carouselId),
-      {
-        alwaysShowTracks: false,
-      });
-    this.scrollbar = scrollbar;
+  const handleClickImage = () => {
+    if (currentImage === images.length - 1) {
+      return;
+    }
+    setCurrentImage(currentImage + 1);
+  };
 
-    const carousel = ReactDOM.findDOMNode(this);
+  const [isDown, setIsDown] = useState(false);
+  const [startX, setStartX] = useState(false);
+  const onMouseDown = (e) => {
+    e.preventDefault();
+    setIsDown(true);
+    setStartX(e.pageX);
+  };
 
-    carousel.addEventListener('mousedown', (e) => {
-      e.preventDefault();
-      this.setState({
-        isDown: true,
-        startX: e.pageX - carousel.offsetLeft,
-        scrollLeft: scrollbar.scrollLeft,
-      });
-    });
+  const onMouseUp = (e) => {
+    setIsDown(false);
+  };
 
-    carousel.addEventListener('mouseleave', () => {
-      this.setState({
-        isDown: false,
-      });
-    });
-    carousel.addEventListener('mouseup', () => {
-      this.setState({
-        isDown: false,
-      });
-    });
-    carousel.addEventListener('mousemove', (e) => {
-      if (!this.state.isDown) {
-        return;  // stop the fn from running
-      }
-      this.setState({
-        isMoving: true,
-      });
-      e.preventDefault();
-      const x = e.pageX - carousel.offsetLeft;
-      const walk = (x - this.state.startX) * 3;
-      scrollbar.scrollTo(this.state.scrollLeft - walk, 0, 600);
-    });
-  }
+  const onMouseMove = (e) => {
+    if (!isDown) {
+      return;  // stop the fn from running
+    }
+    setIsMoving(true);
+    e.preventDefault();
+    const x = e.pageX;
+    const walk = (x-startX) * 3;
+    scrollbar.scrollTo(scrollbar.scrollLeft - walk, 0, 600);
+  };
 
-  goLeft = () => {
-    this.scrollbar.scrollTo(this.state.scrollLeft - window.innerWidth/3, 0, 600);
-  }
-  goRight = () => {
-    this.scrollbar.scrollTo(this.state.scrollLeft + window.innerWidth/3, 0, 600);
-  }
-
-  renderGallery () {
-    const {
-      images
-    } = this.props;
-
-    if (!images) return;
-
-    const gallery = images.map((obj, i) => {
-      return (
-        <div
-          key={i}
-          className="gaming_carousel_object"
-          onLoad={()=>this.scrollbar.update()}
-          onClick={(e)=> this.openLightbox(i,e)}
-        >
-          <img
-            src={obj.src}
-            className="gaming_carousel_image"
-            alt="gaming screenshot"
-          />
-        </div>
-      );
-    });
-
-    return gallery;
-  }
-
-
-  render(){
-
-    return (
-      <div className="gaming_carousel__wrapper">
-        <div className="gaming_carousel__container" id={this.props.carouselId.slice(1)}>
-          {this.renderGallery()}
-          <Lightbox
-            currentImage={this.state.currentImage}
-            images={this.props.images}
-            isOpen={this.state.lightboxIsOpen}
-            onClickImage={this.handleClickImage}
-            onClickNext={this.gotoNext}
-            onClickPrev={this.gotoPrevious}
-            onClickThumbnail={this.gotoImage}
-            onClose={this.closeLightbox}
-            showThumbnails={this.props.showThumbnails}
-            theme={this.props.theme}
-            backdropClosesModal={true}
-          />
-        </div>
-        <div className="indicators">
-          <div onClick={()=>this.goLeft()}>&lsaquo;</div>
-          <div onClick={()=>this.goRight()}>&rsaquo;</div>
-        </div>
+  return (
+    <div className="gaming_carousel__wrapper">
+      <div
+        className="gaming_carousel__container"
+        id={carouselId.slice(1)}
+        onMouseDown={(e) => onMouseDown(e)}
+        onMouseUp={() => onMouseUp()}
+        onMouseLeave={() => onMouseUp()}
+        onMouseMove={(e) => onMouseMove(e)}
+      >
+        <RenderGallery images={images} openLightbox = {openLightbox} scrollbar={scrollbar}/>
+        <Lightbox
+          currentImage={currentImage}
+          images={images}
+          isOpen={lightboxOpen}
+          onClickImage={handleClickImage}
+          onClickNext={()=>setCurrentImage(currentImage + 1)}
+          onClickPrev={()=>setCurrentImage(currentImage - 1)}
+          onClose={closeLightbox}
+          backdropClosesModal={true}
+          openLightbox = {openLightbox}
+        />
       </div>
-    );
-  }
-}
+      <div className="indicators">
+        <div onClick={()=>goLeft()}>&lsaquo;</div>
+        <div onClick={()=>goRight()}>&rsaquo;</div>
+      </div>
+    </div>
+  );
+};
+
+export default Viewer;
---
+++ client/src/components/stash/LightboxViewer.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import Lightbox from 'react-images';
+import Scrollbar from 'smooth-scrollbar';
+import ReactDOM from 'react-dom';
+
+export default class Viewer extends React.Component{
+  state = {
+    lightboxIsOpen: false,
+    currentImage: 0,
+    isDown: false,
+    isMoving: false,
+    startX: 0,
+    scrollLeft: 0,
+  };
+
+  openLightbox = (index, event) =>{
+    if (this.state.isMoving) {
+      event.preventDefault();
+      this.setState({
+        isMoving: false,
+      });
+    }
+    else {
+      event.preventDefault();
+      this.setState({
+        currentImage: index,
+        lightboxIsOpen: true,
+      });
+    }
+  }
+  closeLightbox = () => {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious = () => {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext = () => {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  gotoImage = (index) => {
+    this.setState({
+      currentImage: index,
+    });
+  }
+  handleClickImage = () => {
+    if (this.state.currentImage === this.props.images.length - 1) return;
+
+    this.gotoNext();
+  }
+
+  componentDidMount() {
+    const scrollbar = Scrollbar.init(document.querySelector(this.props.carouselId),
+      {
+        alwaysShowTracks: false,
+      });
+    this.scrollbar = scrollbar;
+
+    const carousel = ReactDOM.findDOMNode(this);
+
+    carousel.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      this.setState({
+        isDown: true,
+        startX: e.pageX - carousel.offsetLeft,
+        scrollLeft: scrollbar.scrollLeft,
+      });
+    });
+
+    carousel.addEventListener('mouseleave', () => {
+      this.setState({
+        isDown: false,
+      });
+    });
+    carousel.addEventListener('mouseup', () => {
+      this.setState({
+        isDown: false,
+      });
+    });
+    carousel.addEventListener('mousemove', (e) => {
+      if (!this.state.isDown) {
+        return;  // stop the fn from running
+      }
+      this.setState({
+        isMoving: true,
+      });
+      e.preventDefault();
+      const x = e.pageX - carousel.offsetLeft;
+      const walk = (x - this.state.startX) * 3;
+      scrollbar.scrollTo(this.state.scrollLeft - walk, 0, 600);
+    });
+  }
+
+  goLeft = () => {
+    this.scrollbar.scrollTo(this.state.scrollLeft - window.innerWidth/3, 0, 600);
+  }
+  goRight = () => {
+    this.scrollbar.scrollTo(this.state.scrollLeft + window.innerWidth/3, 0, 600);
+  }
+
+  renderGallery () {
+    const {
+      images
+    } = this.props;
+
+    if (!images) return;
+
+    const gallery = images.map((obj, i) => {
+      return (
+        <div
+          key={i}
+          className="gaming_carousel_object"
+          onLoad={()=>this.scrollbar.update()}
+          onClick={(e)=> this.openLightbox(i,e)}
+        >
+          <img
+            src={obj.src}
+            className="gaming_carousel_image"
+            alt="gaming screenshot"
+          />
+        </div>
+      );
+    });
+
+    return gallery;
+  }
+
+
+  render(){
+
+    return (
+      <div className="gaming_carousel__wrapper">
+        <div className="gaming_carousel__container" id={this.props.carouselId.slice(1)}>
+          {this.renderGallery()}
+          <Lightbox
+            currentImage={this.state.currentImage}
+            images={this.props.images}
+            isOpen={this.state.lightboxIsOpen}
+            onClickImage={this.handleClickImage}
+            onClickNext={this.gotoNext}
+            onClickPrev={this.gotoPrevious}
+            onClickThumbnail={this.gotoImage}
+            onClose={this.closeLightbox}
+            backdropClosesModal={true}
+          />
+        </div>
+        <div className="indicators">
+          <div onClick={()=>this.goLeft()}>&lsaquo;</div>
+          <div onClick={()=>this.goRight()}>&rsaquo;</div>
+        </div>
+      </div>
+    );
+  }
+}
